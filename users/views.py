4d8e850927809a838b82faa057b3e27250d9e786
MideJayeoba <jayeobapeace19459@gmail.com>
2024-11-08 16:15:13 +0100

    registration and login updated

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
 
@@ -9,8 +9,19 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("posts:list")
+            return redirect("posts_list")
     else:
         messages.warning(request, message="Please make sure you fill the form in a valid way.")
         form = UserCreationForm()
-    return render(request, 'register.html', {"form": form})
+    return render(request, 'users/register.html', {"form": form})
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+
+            return redirect("posts_list")
+    else:
+        form = AuthenticationForm
+    
+    return render(request, 'users/login.html', {"form":form})
