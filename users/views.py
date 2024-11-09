4e525ebb4abcddd2c8713689a281777296e08a50
MideJayeoba <jayeobapeace19459@gmail.com>
2024-11-09 14:42:23 +0100

    logout functionality

--- users/views.py
+++ users/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts_list")
     else:
-        messages.warning(request, message="Please make sure you fill the form in a valid way.")
+        # messages.warning(request, message="Please make sure you fill the form in a valid way.")
         form = UserCreationForm()
     return render(request, 'users/register.html', {"form": form})
 
-def login(request):
+def login_user(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-
+            login(request, form.get_user())
             return redirect("posts_list")
     else:
         form = AuthenticationForm
     
     return render(request, 'users/login.html', {"form":form})
+
+def logout_user(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts_list")
