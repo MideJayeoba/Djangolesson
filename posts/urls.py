437414f9513d5345f333fc495c30ba88c4a3bfb1
MideJayeoba <jayeobapeace19459@gmail.com>
2024-11-15 00:58:00 +0100

    Uptimised home, login, logout functionality

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.posts_list, name="posts_list"),
+    path('new-post/', views.post_new, name="new-post"),
     path('<slug:slug>', views.post_page, name="post_page"),
 ]
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,23 @@
-console.log("This is the javascript side of the divide.");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const hamburger = document.getElementById("hamburgerBtn");
+//   const navLinks = document.getElementById("navLinks");
+//   const mainNav = document.getElementById("mainNav");
+
+//   hamburger.addEventListener("click", function () {
+//     navLinks.classList.toggle("active");
+//     mainNav.classList.toggle("open");
+//     hamburger.classList.toggle("active");
+//   });
+
+//   // Close menu when clicking outside
+//   document.addEventListener("click", function (event) {
+//     if (
+//       !mainNav.contains(event.target) &&
+//       navLinks.classList.contains("active")
+//     ) {
+//       navLinks.classList.remove("active");
+//       mainNav.classList.remove("open");
+//       hamburger.classList.remove("active");
+//     }
+//   });
+// });
